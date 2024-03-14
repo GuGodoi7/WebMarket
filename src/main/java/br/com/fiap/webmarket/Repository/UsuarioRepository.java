91620756d94c23bb3f89b559a6c323af61b157f0
mathabes <matheusoabes@gmail.com>
2024-03-14 09:51:00 -0300

    fix: constructor, etc.

--- src/main/java/br/com/fiap/webmarket/Repository/UsuarioRepository.java
+++ src/main/java/br/com/fiap/webmarket/Repository/UsuarioRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.fiap.webmarket.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario , Long> {
-
+    
 }
     
 
--- src/main/java/br/com/fiap/webmarket/model/Usuario.java
+++ src/main/java/br/com/fiap/webmarket/model/Usuario.java
@@ -4,11 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 @Entity
 public class Usuario{
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
