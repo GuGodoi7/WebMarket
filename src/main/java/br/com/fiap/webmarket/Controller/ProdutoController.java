1a5fc9f6dc3da03e3fa3f897a034aff94907d864
Gustavo Godoi da Silva <gustavogodoi772@gmail.com>
2024-05-07 11:01:57 -0300

    cache

--- src/main/java/br/com/fiap/webmarket/Controller/ProdutoController.java
+++ src/main/java/br/com/fiap/webmarket/Controller/ProdutoController.java
@@ -4,6 +4,9 @@ import static org.springframework.http.HttpStatus.CREATED;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,20 +19,22 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("produto")
+@CacheConfig(cacheNames = "produtos")
 public class ProdutoController {
     
     @Autowired
     ProdutoRepository repository;
 
     @GetMapping
+    @Cacheable()
     public List<Produto> index(){
         return repository.findAll();
     }
 
     @PostMapping
     @ResponseStatus(CREATED)
+    @CacheEvict( allEntries  = true)
     public Produto create(@RequestBody @Valid Produto produto){
         return repository.save(produto);
     }
-
 }
--- src/main/java/br/com/fiap/webmarket/WebmarketApplication.java
+++ src/main/java/br/com/fiap/webmarket/WebmarketApplication.java
@@ -2,12 +2,14 @@ package br.com.fiap.webmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @Controller
+@EnableCaching
 public class WebmarketApplication {
 
 	public static void main(String[] args) {
