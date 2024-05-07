2489e0091072bcb1c6ada866becc1888c4bddd0d
Gustavo Godoi da Silva <gustavogodoi772@gmail.com>
2024-05-07 11:56:22 -0300

    Doc

--- src/main/java/br/com/fiap/webmarket/Controller/UsuarioController.java
+++ src/main/java/br/com/fiap/webmarket/Controller/UsuarioController.java
@@ -1,6 +1,10 @@
 package br.com.fiap.webmarket.Controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
@@ -11,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import br.com.fiap.webmarket.Repository.UsuarioRepository;
 import br.com.fiap.webmarket.model.Usuario;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,18 +30,30 @@ import org.springframework.web.bind.annotation.PutMapping;
 @RestController
 @RequestMapping("usuario")
 @Slf4j
+@CacheConfig(cacheNames =  "usuario")
+@Tag(name = "usuario")
 public class UsuarioController {
     
     @Autowired //Injeção de Dependência
     UsuarioRepository repository;
     
     @GetMapping
+    @Cacheable
+    @Operation(summary = "Cadastrar Usuário",
+    description = "Retyorna um array com os Usuarios"
+    )
+    
     public List<Usuario> index() {
         return repository.findAll();
     }
     
     @PostMapping
     @ResponseStatus(CREATED)
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "Usuario Cadastrado com susseso"),
+        @ApiResponse(responseCode = "400", description = "Falha no cadastro. Verifique os dados digitalizados")
+    })
+    @CacheEvict( allEntries  = true)
     public Usuario create(@RequestBody @Valid Usuario usuario){
         log.info("cadastrando usuario {}", usuario);
         
@@ -42,6 +62,7 @@ public class UsuarioController {
 
     @DeleteMapping("{id}")
     @ResponseStatus(NO_CONTENT)
+    @CacheEvict( allEntries  = true)
     public void destroy(@PathVariable Long id){
         log.info("apagando usuario {}", id);
         verificarSeUsuarioExiste(id);
@@ -49,6 +70,7 @@ public class UsuarioController {
 }
 
     @PutMapping("{id}")
+    @CacheEvict( allEntries  = true) //n sei se tem isso aqui
     public Usuario update(@PathVariable Long id, @RequestBody Usuario usuario){
         log.info("atualizando usuario {} para {}", id, usuario);
         verificarSeUsuarioExiste(id);
--- src/main/java/br/com/fiap/webmarket/WebmarketApplication.java
+++ src/main/java/br/com/fiap/webmarket/WebmarketApplication.java
@@ -7,9 +7,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
 @Controller
 @EnableCaching
+@OpenAPIDefinition (
+	info = @Info(
+		title = "WebMarket",
+		version = "1.0.0",
+		description = "API do app de MarketPlace"
+	)
+)
 public class WebmarketApplication {
 
 	public static void main(String[] args) {
